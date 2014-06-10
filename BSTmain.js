c58c28b3e3890c9fa69d102ce8fa95902de04130
Danish Shaikh <danishshaikh556@gmail.com>
2014-06-10 11:47:25 -0700

    Update BSTmain.js

--- BSTmain.js
+++ BSTmain.js
@@ -7,6 +7,12 @@ tree1.add(3,3);
 tree1.add(45,45);
 tree1.add(45,45);
 
-root = tree1.rootreturn();
+
+root = tree1._root;
 console.log(root.left.data  + "----root--" + root.right.data);
 tree1.inOrder();
+
+var tree2 = new BST();
+tree2.add(5,3);
+
+console.log(tree2.dan);
